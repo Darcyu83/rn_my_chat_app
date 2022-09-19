c007404da8f6cf046d202a68344622d7042932d8
Darcy <darcy.u83@gmail.com>
2022-09-19 23:38:37 +0900

    toggle state before and after promise function

--- src/screens/chat/ChatHome.tsx
+++ src/screens/chat/ChatHome.tsx
@@ -1,5 +1,5 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView, ActivityIndicator } from 'react-native';
+import React, { useRef, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamsList } from '../../../App';
@@ -8,9 +8,46 @@ import NavMenu from '../../components/NavMenu';
 const ChatHome = () => {
     const navigation = useNavigation<NativeStackNavigationProp<RootStackParamsList>>();
 
+    const [isLoading, setIsLoading] = useState(false);
+
+    const promiseFunc = (num: number) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('num----', num % 2);
+                if (num % 2) {
+                    resolve('ok');
+                } else {
+                    reject('ng');
+                }
+            }, 3000);
+        });
+    };
+
+    const asyncFunc = async (num: number) => {
+        setIsLoading(true);
+        const result = await promiseFunc(num).catch((e) => e);
+
+        console.log('result----', result);
+        if (result === 'ok') {
+            setIsLoading(false);
+        } else {
+        }
+    };
+
+    const ref = useRef(0);
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Text>ChatHome</Text>
+            <Text
+                onPress={async () => {
+                    ref.current += 1;
+                    await asyncFunc(ref.current);
+                }}
+                style={{ width: '100%', height: 80, backgroundColor: 'tomato' }}
+            >
+                Check state changed
+            </Text>
+            {isLoading && <ActivityIndicator size={'large'} />}
             <NavMenu />
         </SafeAreaView>
     );
